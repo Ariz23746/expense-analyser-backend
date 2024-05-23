4c33cd4bfcb0a03f10b10a8cc3b9a6a720fdad02
Mohammad Ariz Khan <arizkhan@192.168.1.12>
2024-05-23 21:22:34 +0530

    feat: AsyncHandler, ApiResponse, ApiError handler added

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app = express();
+
+// setting cors to allow communication
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// setting json format to be allowed
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+// allowing express to understand url encoded data ex: ariz%20khan
+app.use(
+  express.urlencoded({
+    limit: "16kb",
+    extended: true,
+  })
+);
+
+// saving temporary files to server in public folder
+app.use(express.static("public"));
+
+// allowing express to access user cookies
+app.use(cookieParser());
+
+export { app };
